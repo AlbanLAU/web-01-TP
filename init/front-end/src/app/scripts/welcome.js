143ceabd9276654dafe85e65089439e44bcfc06b
alauby <alban.lauby@gmail.com>
2024-02-28 16:30:22 +0100

    fix: replace concatenated string

--- init/front-end/src/app/scripts/welcome.js
+++ init/front-end/src/app/scripts/welcome.js
@@ -11,8 +11,7 @@ import { Component } from "./component";
   
       form.addEventListener(
         "submit",
-        // TODO #arrow-function: use arrow function instead.
-        function (event) {
+        (event) => {
           event.preventDefault();
           if (form.checkValidity() === false) {
             event.stopPropagation();
@@ -23,7 +22,7 @@ import { Component } from "./component";
   
             this._startGame(name, size);
           }
-        }.bind(this),
+        },
         false
       );
   
@@ -32,8 +31,7 @@ import { Component } from "./component";
 
     _startGame(name, size) {
       const gamePage = "./#game";
-      // TODO #template-literals:  use template literals (backquotes)
-      window.location = gamePage + "?name=" + name + "&size=" + size;
+      window.location = `${gamePage}?name=${name}&size=${size}`;
     }
 
   }
