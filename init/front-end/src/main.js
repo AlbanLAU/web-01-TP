08b0d35815707be9319b9f916728695f9109e9aa
alauby <alban.lauby@gmail.com>
2024-02-28 14:49:05 +0100

    feat: implementation of css import management

--- init/front-end/src/main.js
+++ init/front-end/src/main.js
@@ -4,6 +4,9 @@ import WelcomeComponent from "./app/scripts/welcome.js";
 import GameComponent from "./app/scripts/game.js";
 import ScoreComponent from "./app/scripts/score.js";
 
+import "/node_modules/bootstrap/dist/css/bootstrap.css";
+import "./app/styles/style.css";
+
 const outlet = document.querySelector("#content-outlet");
 const router = new Router(outlet);
 router
